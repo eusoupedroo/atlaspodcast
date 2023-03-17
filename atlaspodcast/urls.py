6b00c1d8c8a61f59aa51c2516f00d734edd131ca
Pedro Sousa <jpedroleitedesousa@gmail.com>
2023-03-17 16:28:13 -0300

    Url Page Project Redirectioner

--- atlaspodcast/urls.py
+++ atlaspodcast/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('podcast.urls')),
-    # path('/', include('podcast.urls')),
 ]  
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
